9f1aae223fddf05bc78ef2ceb08b6e07d6dbb013
Grass <nxiao49566@gmail.com>
2026-09-05 20:32:54 +0900

    fix

--- SakusakuMain/Cogs/Commands/Public/Utility/ping.py
+++ SakusakuMain/Cogs/Commands/Public/Utility/ping.py
@@ -1,4 +1,4 @@
-import discord
+import discord 
 from discord.ext import commands
 
 class Ping(commands.Cog):
@@ -8,7 +8,7 @@ class Ping(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)
-        await ctx.send(f"🏓 Pong! `{latency}ms`")
+        await ctx.send(f"Pong!:ping_pong:\nBotのPing値は{latency}です。")
 
 async def setup(bot: commands.Cog):
     await bot.add_cog(Ping(bot))
